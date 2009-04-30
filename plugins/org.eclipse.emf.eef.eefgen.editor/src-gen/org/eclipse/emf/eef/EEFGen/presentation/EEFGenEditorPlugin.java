2f9285c6dd8ae04f4970ea9a1c1e41db75aa6808
glefur <glefur>
2009-04-30 17:13:14 +0000

    First release of EEF project

---
+++ plugins/org.eclipse.emf.eef.eefgen.editor/src-gen/org/eclipse/emf/eef/EEFGen/presentation/EEFGenEditorPlugin.java
@@ -0,0 +1,117 @@
+/**
+ *  Copyright (c) 2008 Obeo.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *  Contributors:
+ *      Obeo - initial API and implementation
+ * 
+ *
+ * $Id: EEFGenEditorPlugin.java,v 1.1 2009/04/30 17:13:14 glefur Exp $
+ */
+package org.eclipse.emf.eef.EEFGen.presentation;
+
+import org.eclipse.emf.codegen.ecore.genmodel.provider.GenModelEditPlugin;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
+
+import org.eclipse.emf.eef.components.provider.ComponentsEditPlugin;
+
+import org.eclipse.emf.eef.mapping.provider.MappingEditPlugin;
+
+import org.eclipse.emf.eef.views.provider.ViewsEditPlugin;
+
+/**
+ * This is the central singleton for the EEFGen editor plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class EEFGenEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final EEFGenEditorPlugin INSTANCE = new EEFGenEditorPlugin();
+	
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EEFGenEditorPlugin() {
+		super
+			(new ResourceLocator [] {
+				ComponentsEditPlugin.INSTANCE,
+				EcoreEditPlugin.INSTANCE,
+				GenModelEditPlugin.INSTANCE,
+				MappingEditPlugin.INSTANCE,
+				ViewsEditPlugin.INSTANCE,
+			});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+	
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
