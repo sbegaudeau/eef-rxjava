faaceba302833484e724f5bce02a69b112b24321
sbouchet <sbouchet>
2009-05-18 16:06:01 +0000

    removed unused imports

--- plugins/org.eclipse.emf.eef.eefgen.edit/src-gen/org/eclipse/emf/eef/EEFGen/providers/EEFGenModelReferencePropertiesEditionProvider.java
+++ plugins/org.eclipse.emf.eef.eefgen.edit/src-gen/org/eclipse/emf/eef/EEFGen/providers/EEFGenModelReferencePropertiesEditionProvider.java
@@ -9,13 +9,14 @@
  *      Obeo - initial API and implementation
  * 
  *
- * $Id: EEFGenModelReferencePropertiesEditionProvider.java,v 1.2 2009/05/05 12:06:10 sbouchet Exp $
+ * $Id: EEFGenModelReferencePropertiesEditionProvider.java,v 1.3 2009/05/18 16:06:01 sbouchet Exp $
  */
 package org.eclipse.emf.eef.EEFGen.providers;
 
 import org.eclipse.emf.ecore.EObject;
 
 import org.eclipse.emf.eef.runtime.api.component.IPropertiesEditionComponent;
+import org.eclipse.emf.eef.runtime.api.parts.IPropertiesEditionPart;
 import org.eclipse.emf.eef.runtime.api.providers.IPropertiesEditionProvider;
 
 import org.eclipse.emf.eef.EEFGen.EEFGenPackage;
