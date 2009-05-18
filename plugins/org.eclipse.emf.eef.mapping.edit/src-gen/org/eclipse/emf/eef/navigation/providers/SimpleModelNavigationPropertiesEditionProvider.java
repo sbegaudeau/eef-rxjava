20f22382b7fc46252f5bb67d3947d1778fd96fd8
sbouchet <sbouchet>
2009-05-18 16:14:47 +0000

    removed unused imports

--- plugins/org.eclipse.emf.eef.mapping.edit/src-gen/org/eclipse/emf/eef/navigation/providers/SimpleModelNavigationPropertiesEditionProvider.java
+++ plugins/org.eclipse.emf.eef.mapping.edit/src-gen/org/eclipse/emf/eef/navigation/providers/SimpleModelNavigationPropertiesEditionProvider.java
@@ -9,13 +9,14 @@
  *      Obeo - initial API and implementation
  * 
  *
- * $Id: SimpleModelNavigationPropertiesEditionProvider.java,v 1.2 2009/05/05 12:07:32 sbouchet Exp $
+ * $Id: SimpleModelNavigationPropertiesEditionProvider.java,v 1.3 2009/05/18 16:14:47 sbouchet Exp $
  */
 package org.eclipse.emf.eef.navigation.providers;
 
 import org.eclipse.emf.ecore.EObject;
 
 import org.eclipse.emf.eef.runtime.api.component.IPropertiesEditionComponent;
+import org.eclipse.emf.eef.runtime.api.parts.IPropertiesEditionPart;
 import org.eclipse.emf.eef.runtime.api.providers.IPropertiesEditionProvider;
 
 import org.eclipse.emf.eef.mapping.navigation.NavigationPackage;
