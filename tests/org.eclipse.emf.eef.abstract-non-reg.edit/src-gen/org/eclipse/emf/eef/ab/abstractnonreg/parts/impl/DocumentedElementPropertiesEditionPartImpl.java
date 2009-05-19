4bf143d315f1143bce7d09abc8a7b05ed6a8ad0c
sbouchet <sbouchet>
2009-05-19 13:56:21 +0000

    regen with latest EEF templates

--- tests/org.eclipse.emf.eef.abstract-non-reg.edit/src-gen/org/eclipse/emf/eef/ab/abstractnonreg/parts/impl/DocumentedElementPropertiesEditionPartImpl.java
+++ tests/org.eclipse.emf.eef.abstract-non-reg.edit/src-gen/org/eclipse/emf/eef/ab/abstractnonreg/parts/impl/DocumentedElementPropertiesEditionPartImpl.java
@@ -39,7 +39,7 @@ public class DocumentedElementPropertiesEditionPartImpl extends CompositePropert
 	public DocumentedElementPropertiesEditionPartImpl(IPropertiesEditionComponent editionComponent) {
 		super(editionComponent);
 	}
-	
+
 	public Composite createFigure(final Composite parent) {
 		view = new Composite(parent, SWT.NONE);
 		GridLayout layout = new GridLayout();
@@ -49,13 +49,13 @@ public class DocumentedElementPropertiesEditionPartImpl extends CompositePropert
 		createControls(view);
 		return view;
 	}
-	
+
 	public void createControls(Composite view) { 
 		createDocumentationGroup(view);
-		
+
 		// Start of user code for additional ui definition
 		
-		// End of user code		
+		// End of user code
 	}
 
 	protected void createDocumentationGroup(Composite parent) {
@@ -82,11 +82,11 @@ public class DocumentedElementPropertiesEditionPartImpl extends CompositePropert
 		SWTUtils.createHelpButton(parent, propertiesEditionComponent.getHelpContent(AbstractnonregViewsRepository.DocumentedElement.documentation, AbstractnonregViewsRepository.SWT_KIND), null); //$NON-NLS-1$
 	}
 
-	
+
 	public void firePropertiesChanged(PropertiesEditionEvent event) {
 		// Start of user code for tab synchronization
 		
-		// End of user code		
+		// End of user code
 	}
 
 	/**
