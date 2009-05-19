75ef742f81e68c74bc9de647b257743605f50c7a
sbouchet <sbouchet>
2009-05-19 07:57:37 +0000

    regen with latest EEF templates

--- plugins/org.eclipse.emf.eef.mapping.edit/src-gen/org/eclipse/emf/eef/mapping/components/CategoryBasePropertiesEditionComponent.java
+++ plugins/org.eclipse.emf.eef.mapping.edit/src-gen/org/eclipse/emf/eef/mapping/components/CategoryBasePropertiesEditionComponent.java
@@ -9,13 +9,11 @@
  *      Obeo - initial API and implementation
  * 
  *
- * $Id: CategoryBasePropertiesEditionComponent.java,v 1.3 2009/05/05 12:07:29 sbouchet Exp $
+ * $Id: CategoryBasePropertiesEditionComponent.java,v 1.4 2009/05/19 07:57:37 sbouchet Exp $
  */
 package org.eclipse.emf.eef.mapping.components;
 
 // Start of user code for imports
-
-import java.util.ArrayList;
 
 import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.common.command.UnexecutableCommand;
@@ -35,6 +33,7 @@ import org.eclipse.emf.eef.mapping.MappingPackage;
 import org.eclipse.emf.eef.mapping.parts.CategoryPropertiesEditionPart;
 import org.eclipse.emf.eef.mapping.parts.MappingViewsRepository;
 import org.eclipse.emf.eef.runtime.api.component.IPropertiesEditionComponent;
+import org.eclipse.emf.eef.runtime.api.notify.IPropertiesEditionListener;
 import org.eclipse.emf.eef.runtime.api.parts.IPropertiesEditionPart;
 import org.eclipse.emf.eef.runtime.api.providers.IPropertiesEditionPartProvider;
 import org.eclipse.emf.eef.runtime.impl.components.StandardPropertiesEditionComponent;
@@ -74,7 +73,6 @@ public class CategoryBasePropertiesEditionComponent extends StandardPropertiesEd
 				this.category.eAdapters().add(semanticAdapter);
 			}
 		}
-		listeners = new ArrayList();
 		this.editing_mode = editing_mode;
 	}
 	
@@ -112,7 +110,6 @@ public class CategoryBasePropertiesEditionComponent extends StandardPropertiesEd
 			return MappingViewsRepository.Category.class;
 		return super.translatePart(key);
 	}
-	
 
 	/**
 	 * {@inheritDoc}
@@ -135,7 +132,7 @@ public class CategoryBasePropertiesEditionComponent extends StandardPropertiesEd
 				IPropertiesEditionPartProvider provider = PropertiesEditionPartProviderService.getInstance().getProvider(MappingViewsRepository.class);
 				if (provider != null) {
 					basePart = (CategoryPropertiesEditionPart)provider.getPropertiesEditionPart(MappingViewsRepository.Category.class, kind, this);
-					listeners.add(basePart);
+					addListener((IPropertiesEditionListener)basePart);
 				}
 			}
 			return (IPropertiesEditionPart)basePart;
@@ -153,10 +150,17 @@ public class CategoryBasePropertiesEditionComponent extends StandardPropertiesEd
 		if (basePart != null && key == MappingViewsRepository.Category.class) {
 			((IPropertiesEditionPart)basePart).setContext(elt, allResource);
 			Category category = (Category)elt;
+			// init values
 			if (category.getName() != null)
 				basePart.setName(category.getName());
 
+			
+			// init filters
+			
 		}
+		// init values for referenced views
+
+		// init filters for referenced views
 
 	}
 
@@ -212,8 +216,7 @@ public class CategoryBasePropertiesEditionComponent extends StandardPropertiesEd
 
 
 
-			if (command != null)
-				liveEditingDomain.getCommandStack().execute(command);
+			liveEditingDomain.getCommandStack().execute(command);
 		} else if (PropertiesEditionEvent.CHANGE == event.getState()) {
 			Diagnostic diag = this.validateValue(event);
 			if (diag != null && diag.getSeverity() != Diagnostic.OK) {
