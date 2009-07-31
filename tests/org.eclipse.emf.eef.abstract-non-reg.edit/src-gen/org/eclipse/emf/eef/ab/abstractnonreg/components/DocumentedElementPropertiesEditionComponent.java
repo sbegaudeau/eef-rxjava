d942567735c8f3d14a5ee780c22f11ecb2d0cdc8
glefur <glefur>
2009-07-31 14:34:01 +0000

    Synchronized code for new generation

--- tests/org.eclipse.emf.eef.abstract-non-reg.edit/src-gen/org/eclipse/emf/eef/ab/abstractnonreg/components/DocumentedElementPropertiesEditionComponent.java
+++ tests/org.eclipse.emf.eef.abstract-non-reg.edit/src-gen/org/eclipse/emf/eef/ab/abstractnonreg/components/DocumentedElementPropertiesEditionComponent.java
@@ -249,7 +249,7 @@ public class DocumentedElementPropertiesEditionComponent extends StandardPropert
 
 
 
-			if (!command.canExecute()) {
+			if (!command.isEmpty() && !command.canExecute()) {
 				EMFPropertiesRuntime.getDefault().logError("Cannot perform model change command.", null);
 			} else {
 				liveEditingDomain.getCommandStack().execute(command);
