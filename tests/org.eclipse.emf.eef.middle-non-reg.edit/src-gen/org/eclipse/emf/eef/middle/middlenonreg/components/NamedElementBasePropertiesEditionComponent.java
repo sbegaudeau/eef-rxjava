46d0f47ce4db500287c668c4488dc9d428891f3a
glefur <glefur>
2009-07-31 14:34:03 +0000

    Synchronized code for new generation

--- tests/org.eclipse.emf.eef.middle-non-reg.edit/src-gen/org/eclipse/emf/eef/middle/middlenonreg/components/NamedElementBasePropertiesEditionComponent.java
+++ tests/org.eclipse.emf.eef.middle-non-reg.edit/src-gen/org/eclipse/emf/eef/middle/middlenonreg/components/NamedElementBasePropertiesEditionComponent.java
@@ -241,7 +241,7 @@ public class NamedElementBasePropertiesEditionComponent extends StandardProperti
 
 
 
-			if (!command.canExecute()) {
+			if (!command.isEmpty() && !command.canExecute()) {
 				EMFPropertiesRuntime.getDefault().logError("Cannot perform model change command.", null);
 			} else {
 				liveEditingDomain.getCommandStack().execute(command);
