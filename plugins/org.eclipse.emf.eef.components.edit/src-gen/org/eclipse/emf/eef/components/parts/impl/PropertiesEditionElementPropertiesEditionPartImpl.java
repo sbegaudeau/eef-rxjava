39ff7a6cbfdcf01f7b92a1fde4ac20e16b3f7e13
glefur <glefur>
2009-08-05 13:09:40 +0000

    UPDATED: column width, size from MPO

--- plugins/org.eclipse.emf.eef.components.edit/src-gen/org/eclipse/emf/eef/components/parts/impl/PropertiesEditionElementPropertiesEditionPartImpl.java
+++ plugins/org.eclipse.emf.eef.components.edit/src-gen/org/eclipse/emf/eef/components/parts/impl/PropertiesEditionElementPropertiesEditionPartImpl.java
@@ -9,7 +9,7 @@
  *      Obeo - initial API and implementation
  * 
  *
- * $Id: PropertiesEditionElementPropertiesEditionPartImpl.java,v 1.11 2009/07/31 14:14:18 glefur Exp $
+ * $Id: PropertiesEditionElementPropertiesEditionPartImpl.java,v 1.12 2009/08/05 13:09:40 glefur Exp $
  */
 package org.eclipse.emf.eef.components.parts.impl;
 
@@ -248,7 +248,7 @@ public class PropertiesEditionElementPropertiesEditionPartImpl extends Composite
 		// Start of user code for table views s columns definition
 		
 		TableColumn name = new TableColumn(table, SWT.NONE);
-		name.setWidth(80);
+		name.setWidth(200);
 		name.setText("Views"); //$NON-NLS-1$
 		// End of user code
 		
