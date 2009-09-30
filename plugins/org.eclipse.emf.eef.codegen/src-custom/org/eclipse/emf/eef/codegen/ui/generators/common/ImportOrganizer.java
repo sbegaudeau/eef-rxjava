a5ece5c924c2337ced75d00b4018fcdac20e70d5
glefur <glefur>
2009-09-30 14:00:52 +0000

    REMOVED: Unused variable ADDED: Legal information

--- plugins/org.eclipse.emf.eef.codegen/src-custom/org/eclipse/emf/eef/codegen/ui/generators/common/ImportOrganizer.java
+++ plugins/org.eclipse.emf.eef.codegen/src-custom/org/eclipse/emf/eef/codegen/ui/generators/common/ImportOrganizer.java
@@ -1,6 +1,13 @@
-/**
+/*******************************************************************************
+ * Copyright (c) 2008-2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.codegen.ui.generators.common;
 
 import java.util.ArrayList;
@@ -20,8 +27,7 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IWorkbenchSite;
 
 /**
- * @author glefur
- *
+ * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
  */
 public class ImportOrganizer {
 	
@@ -29,7 +35,6 @@ public class ImportOrganizer {
 		List<ICompilationUnit> units = new ArrayList<ICompilationUnit>();
 		for (IContainer container : folders) {
 			IJavaElement element = JavaCore.create(container);
-			IPackageFragmentRoot[] allPackageFragmentRoots;
 			try {
 				if (element instanceof IPackageFragmentRoot) {
 					IJavaElement[] children = ((IPackageFragmentRoot)element).getChildren();
