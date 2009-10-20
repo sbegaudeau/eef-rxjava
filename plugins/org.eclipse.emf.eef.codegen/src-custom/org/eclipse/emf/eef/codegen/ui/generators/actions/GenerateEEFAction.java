79d47721b73c9200ab1b036c55295bd8aec23c41
sbouchet <sbouchet>
2009-10-20 10:21:10 +0000

    cleanup ( +force hudson build )

--- plugins/org.eclipse.emf.eef.codegen/src-custom/org/eclipse/emf/eef/codegen/ui/generators/actions/GenerateEEFAction.java
+++ plugins/org.eclipse.emf.eef.codegen/src-custom/org/eclipse/emf/eef/codegen/ui/generators/actions/GenerateEEFAction.java
@@ -11,17 +11,10 @@
 package org.eclipse.emf.eef.codegen.ui.generators.actions;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.List;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -30,17 +23,6 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.emf.eef.EEFGen.EEFGenModel;
-import org.eclipse.emf.eef.codegen.EEFCodegenPlugin;
-import org.eclipse.emf.eef.codegen.ui.generators.common.GenerateAll;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.ProgressMonitorDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IActionDelegate;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
 
 /**
  * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
