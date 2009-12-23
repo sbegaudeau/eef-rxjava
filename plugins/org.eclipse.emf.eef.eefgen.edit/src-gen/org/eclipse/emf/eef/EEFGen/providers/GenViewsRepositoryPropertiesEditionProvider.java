fedd015074b25eef90da7c17037b4d537d8a6e41
nlepine <nlepine>
2009-12-23 15:59:22 +0000

    add provide method in providers

--- plugins/org.eclipse.emf.eef.eefgen.edit/src-gen/org/eclipse/emf/eef/EEFGen/providers/GenViewsRepositoryPropertiesEditionProvider.java
+++ plugins/org.eclipse.emf.eef.eefgen.edit/src-gen/org/eclipse/emf/eef/EEFGen/providers/GenViewsRepositoryPropertiesEditionProvider.java
@@ -32,6 +32,15 @@ public class GenViewsRepositoryPropertiesEditionProvider implements IPropertiesE
 	public boolean provides(EObject eObject) {
 		return (eObject instanceof GenViewsRepository) && (EEFGenPackage.eINSTANCE.getGenViewsRepository() == eObject.eClass());
 	}
+	
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.eef.runtime.api.providers.IPropertiesEditionProvider#provides(org.eclipse.emf.ecore.EObject, java.lang.String)
+	 */
+	public boolean provides(EObject eObject, String part) {
+		return provides(eObject) && (GenViewsRepositoryPropertiesEditionComponent.BASE_PART.equals(part));
+	}
 
 	/**
 	 * {@inheritDoc}
