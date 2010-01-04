fb122caf2303cc82a9fa0b7476a7a781859d3d47
sbouchet <sbouchet>
2010-01-04 14:45:06 +0000

    fixed promote script

--- releng/bash-promote-I.sh
+++ releng/bash-promote-I.sh
@@ -14,6 +14,6 @@ cd $BUILD_LOC
 wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-integration/lastSuccessfulBuild/artifact/build/*zip*/build.zip" -o $logFile
 if [ ! -f build.zip ]; then echo "ERROR:build.zip (from Hudson) not found" >> $logFile; exit -2; fi
 unzip build.zip >> $logFile
-echo "[`date +%Y/%m/%d\ %H:%M`]: publishing nightly build ..." >> $logFile
+echo "[`date +%Y/%m/%d\ %H:%M`]: publishing integration build ..." >> $logFile
 mkdir -p $PROMO_LOGS_DIR
 $ANT -f $RELENG_DIR/promote.xml -Dpromote.properties=$RELENG_DIR/promote-I.properties 2>/dev/null 1> $PROMO_LOGS_DIR/cbi-emf-eef-promo-I.txt
