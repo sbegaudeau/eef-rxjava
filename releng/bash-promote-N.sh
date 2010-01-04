62e07c9eb6a70c66b6756e467f572cc41837f072
sbouchet <sbouchet>
2010-01-04 12:26:33 +0000

    fixed promote script

--- releng/bash-promote-N.sh
+++ releng/bash-promote-N.sh
@@ -2,6 +2,7 @@
 BUILD_LOC="/home/data/users/sbouchet"
 PROMO_LOGS_DIR="/home/data/users/sbouchet/promo_logs"
 RELENG_DIR="/opt/users/hudsonbuild/.hudson/jobs/cbi-emf-eef-0.7-nightly/workspace/build/org.eclipse.eef.releng"
+ANT="/opt/public/common/apache-ant-1.7.1/bin/ant"
 logFile="/home/data/users/sbouchet/cronLog.txt"
 
 echo "[`date +%Y/%m/%d\ %H:%M`]: getting last successful build" >> $logFile
@@ -9,9 +10,9 @@ mkdir -p $BUILD_LOC
 rm -f $BUILD_LOC/build.zip
 rm -rf $BUILD_LOC/build
 cd $BUILD_LOC
-wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-nightly/lastSuccessfulBuild/artifact/build/*zip*/build.zip"
+wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-nightly/lastSuccessfulBuild/artifact/build/*zip*/build.zip" >> $logFile
 if [ ! -f build.zip ]; then echo "ERROR:build.zip (from Hudson) not found" >> $logFile; exit -2; fi
-unzip build.zip
+unzip build.zip >> $logFile
 echo "[`date +%Y/%m/%d\ %H:%M`]: publishing nightly build ..." >> $logFile
 mkdir -p $PROMO_LOGS_DIR
 $ANT -f $RELENG_DIR/promote.xml -Dpromote.properties=$RELENG_DIR/promote-N.properties 2>/dev/null 1> $PROMO_LOGS_DIR/cbi-emf-eef-promo-N.txt
