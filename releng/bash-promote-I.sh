12882334ad83dd9163718c823de6e585d6a8ffde
sbouchet <sbouchet>
2010-01-04 14:10:15 +0000

    fixed promote script

--- releng/bash-promote-I.sh
+++ releng/bash-promote-I.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BUILD_LOC="/home/data/users/sbouchet/tmp/"
 PROMO_LOGS_DIR="/home/data/users/sbouchet/promo_logs"
-RELENG_DIR="/opt/users/hudsonbuild/.hudson/jobs/cbi-emf-eef-0.7-nightly/workspace/build/org.eclipse.eef.releng"
+RELENG_DIR="/opt/users/hudsonbuild/.hudson/jobs/cbi-emf-eef-0.7-integration/workspace/build/org.eclipse.eef.releng"
 ANT="/opt/public/common/apache-ant-1.7.1/bin/ant"
 logFile="/home/data/users/sbouchet/cronLog.txt"
 
@@ -11,9 +11,9 @@ mkdir -p $BUILD_LOC
 rm -f $BUILD_LOC/build.zip
 rm -rf $BUILD_LOC/build
 cd $BUILD_LOC
-wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-nightly/lastSuccessfulBuild/artifact/build/*zip*/build.zip" -o $logFile
+wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-integration/lastSuccessfulBuild/artifact/build/*zip*/build.zip" -o $logFile
 if [ ! -f build.zip ]; then echo "ERROR:build.zip (from Hudson) not found" >> $logFile; exit -2; fi
 unzip build.zip >> $logFile
 echo "[`date +%Y/%m/%d\ %H:%M`]: publishing nightly build ..." >> $logFile
 mkdir -p $PROMO_LOGS_DIR
-$ANT -f $RELENG_DIR/promote.xml -Dpromote.properties=$RELENG_DIR/promote-N.properties 2>/dev/null 1> $PROMO_LOGS_DIR/cbi-emf-eef-promo-N.txt
+$ANT -f $RELENG_DIR/promote.xml -Dpromote.properties=$RELENG_DIR/promote-I.properties 2>/dev/null 1> $PROMO_LOGS_DIR/cbi-emf-eef-promo-I.txt
