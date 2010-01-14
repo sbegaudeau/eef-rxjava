af4b199ce8ad34030928a4648904f6ce135cade6
sbouchet <sbouchet>
2010-01-14 14:40:39 +0000

    FIXED : https://bugs.eclipse.org/bugs/show_bug.cgi?id=299470

--- releng/bash-promote-I.sh
+++ releng/bash-promote-I.sh
@@ -11,7 +11,7 @@ mkdir -p $BUILD_LOC
 rm -f $BUILD_LOC/build.zip
 rm -rf $BUILD_LOC/build
 cd $BUILD_LOC
-wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-integration/lastSuccessfulBuild/artifact/build/*zip*/build.zip" -o $logFile
+wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-integration/lastSuccessfulBuild/artifact/build/athena/*zip*/build.zip" -o $logFile
 if [ ! -f build.zip ]; then echo "ERROR:build.zip (from Hudson) not found" >> $logFile; exit -2; fi
 unzip build.zip >> $logFile
 echo "[`date +%Y/%m/%d\ %H:%M`]: publishing integration build ..." >> $logFile
