4089434c44457ca1b0845e7759d6b048a4fc9196
sbouchet <sbouchet>
2010-01-15 14:32:02 +0000

    fixed promote script

--- releng/bash-promote-I.sh
+++ releng/bash-promote-I.sh
@@ -9,12 +9,12 @@ rm -f $logFile
 touch $logFile
 echo "[`date +%Y/%m/%d\ %H:%M`]: getting last successful build" >> $logFile
 mkdir -p $BUILD_LOC
-rm -f $BUILD_LOC/athena.zip
-rm -rf $BUILD_LOC/athena
+rm -f $BUILD_LOC/build.zip
+rm -rf $BUILD_LOC/build
 cd $BUILD_LOC
-wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-integration/lastSuccessfulBuild/artifact/build/athena/*zip*/athena.zip" -o $logFile
-if [ ! -f athena.zip ]; then echo "ERROR:athena.zip (from Hudson) not found" >> $logFile; exit -2; fi
-unzip -o athena.zip >> $logFile
+wget --no-check-certificate "https://build.eclipse.org/hudson/job/cbi-emf-eef-0.7-integration/lastSuccessfulBuild/artifact/build/*zip*/build.zip" -o $logFile
+if [ ! -f build.zip ]; then echo "ERROR:build.zip (from Hudson) not found" >> $logFile; exit -2; fi
+unzip -o build.zip >> $logFile
 echo "[`date +%Y/%m/%d\ %H:%M`]: publishing integration build ..." >> $logFile
 mkdir -p $PROMO_LOGS_DIR
 $ANT -f $RELENG_DIR/promote.xml -Dpromote.properties=$RELENG_DIR/promote-I.properties 2>/dev/null 1> $PROMO_LOGS_DIR/cbi-emf-eef-promo-I.txt
