1998276327ac93559b2744ac0d8750e769f549a0
nlepine <nlepine>
2010-02-08 16:43:48 +0000

    update widget for removing

--- tests/org.eclipse.emf.eef.tests.nonreg.junit/src-gen/org/eclipse/emf/eef/eefnr/tests/junit/EObjectFlatComboViewerSampleTestCase.java
+++ tests/org.eclipse.emf.eef.tests.nonreg.junit/src-gen/org/eclipse/emf/eef/eefnr/tests/junit/EObjectFlatComboViewerSampleTestCase.java
@@ -55,7 +55,7 @@ public class EObjectFlatComboViewerSampleTestCase extends SWTBotEEFTestCase {
 	/**
 	 * The eObjects list contained in widgets
 	 */
-	private List<EObject> allInstancesOf;
+	private List allInstancesOf;
 	/**
 	 * Updated value of the feature
 	 */
@@ -236,6 +236,69 @@ public class EObjectFlatComboViewerSampleTestCase extends SWTBotEEFTestCase {
 		deleteModels();
 	
 	}
+	/**
+	 * Create the expected model from the input model
+	 * @throws InputModelInvalidException error during expected model initialization
+	 * @throws IOException error during expected model serialization
+	 */
+	protected void initializeRemoveExpectedModelForEObjectFlatComboViewerSampleEobjectflatcomboviewerOptionalPropery() throws InputModelInvalidException, IOException {
+		// Create the expected model content by applying the attempted command on a copy of the input model content
+		createExpectedModel();
+		EObject eObjectFlatComboViewerSample = EEFTestsModelsUtils.getFirstInstanceOf(expectedModel, eObjectFlatComboViewerSampleMetaClass);
+		if (eObjectFlatComboViewerSample == null)
+			throw new InputModelInvalidException(eObjectFlatComboViewerSampleMetaClass.getName());
+		CompoundCommand cc = new CompoundCommand();
+		
+		allInstancesOf = EEFTestsModelsUtils.getAllInstancesOf(expectedModel, totalSampleMetaClass);
+		cc.append(SetCommand.create(editingDomain, eObjectFlatComboViewerSample, EefnrPackage.eINSTANCE.getEObjectFlatComboViewerSample_EobjectflatcomboviewerOptionalPropery(), null));
+		editingDomain.getCommandStack().execute(cc);
+		expectedModel.save(Collections.EMPTY_MAP);
+	}
+	/**
+	 * Test the editor properties :
+	 * - init the input model
+	 * - calculate the expected model
+	 * - initialize the model editor
+	 * - change the properties in the editor properties
+	 * - compare the expected and the real model : if they are equals the test pass
+	 * - delete the models
+	 */	
+	public void testRemoveEObjectFlatComboViewerSampleEobjectflatcomboviewerOptionalPropery() throws Exception {
+		
+		// Import the input model
+		initializeInputModel();
+		
+		eObjectFlatComboViewerSample = EEFTestsModelsUtils.getFirstInstanceOf(bot.getActiveResource(), eObjectFlatComboViewerSampleMetaClass);
+		if (eObjectFlatComboViewerSample == null)
+			throw new InputModelInvalidException(eObjectFlatComboViewerSampleMetaClass.getName());
+	
+		// Create the expected model
+		initializeRemoveExpectedModelForEObjectFlatComboViewerSampleEobjectflatcomboviewerOptionalPropery();
+		
+		// Open the input model with the treeview editor
+		SWTBotEditor modelEditor = bot.openActiveModel();
+		
+		// Open the EEF wizard (by double click) to edit the EObjectFlatComboViewerSample element
+		EObject firstInstanceOf = EEFTestsModelsUtils.getFirstInstanceOf(bot.getActiveResource(), eObjectFlatComboViewerSampleMetaClass);
+		if (firstInstanceOf == null)
+			throw new InputModelInvalidException(eObjectFlatComboViewerSampleMetaClass.getName());
+		
+		SWTBotShell wizardShell = bot.prepareBatchEditing(modelEditor, eObjectFlatComboViewerSampleMetaClass, firstInstanceOf);
+		
+		// Change value of the eobjectflatcomboviewerOptionalPropery feature of the EObjectFlatComboViewerSample element 
+		bot.removeEObjectFlatComboViewerFeature(wizardShell, 1);
+	
+		
+		// Save the changement
+		bot.finalizeEdition(modelEditor);
+		
+		// Compare real model with expected model
+		assertExpectedModelReached(expectedModel);
+		
+		// Delete the input model
+		deleteModels();
+	
+	}	
 		// FIXME : define 'additionnalMethodsForWidgets' (from widgetTest.mtl) for case (EObjectFlatComboViewer - TotalSample) 
 
 		// FIXME : define 'additionnalMethodsForWidgets' (from widgetTest.mtl) for case (EObjectFlatComboViewer - TotalSample) 
--- tests/org.eclipse.emf.eef.tests.nonreg.junit/src-gen/org/eclipse/emf/eef/eefnr/tests/junit/properties/EObjectFlatComboViewerSamplePropertiesTestCase.java
+++ tests/org.eclipse.emf.eef.tests.nonreg.junit/src-gen/org/eclipse/emf/eef/eefnr/tests/junit/properties/EObjectFlatComboViewerSamplePropertiesTestCase.java
@@ -55,7 +55,7 @@ public class EObjectFlatComboViewerSamplePropertiesTestCase extends SWTBotEEFTes
 	/**
 	 * The eObjects list contained in widgets
 	 */
-	private List<EObject> allInstancesOf;
+	private List allInstancesOf;
 	/**
 	 * Updated value of the feature
 	 */
@@ -172,7 +172,7 @@ public class EObjectFlatComboViewerSamplePropertiesTestCase extends SWTBotEEFTes
 		// Delete the input model
 		deleteModels();
 	
-	}
+	}	
 	/**
 	 * Create the expected model from the input model
 	 * @throws InputModelInvalidException error during expected model initialization
@@ -235,7 +235,70 @@ public class EObjectFlatComboViewerSamplePropertiesTestCase extends SWTBotEEFTes
 		// Delete the input model
 		deleteModels();
 	
+	}	
+	/**
+	 * Create the expected model from the input model
+	 * @throws InputModelInvalidException error during expected model initialization
+	 * @throws IOException error during expected model serialization
+	 */
+	protected void initializeRemoveExpectedModelForEObjectFlatComboViewerSampleEobjectflatcomboviewerOptionalPropery() throws InputModelInvalidException, IOException {
+		// Create the expected model content by applying the attempted command on a copy of the input model content
+		createExpectedModel();
+		EObject eObjectFlatComboViewerSample = EEFTestsModelsUtils.getFirstInstanceOf(expectedModel, eObjectFlatComboViewerSampleMetaClass);
+		if (eObjectFlatComboViewerSample == null)
+			throw new InputModelInvalidException(eObjectFlatComboViewerSampleMetaClass.getName());
+		CompoundCommand cc = new CompoundCommand();
+		
+		allInstancesOf = EEFTestsModelsUtils.getAllInstancesOf(expectedModel, totalSampleMetaClass);
+		cc.append(SetCommand.create(editingDomain, eObjectFlatComboViewerSample, EefnrPackage.eINSTANCE.getEObjectFlatComboViewerSample_EobjectflatcomboviewerOptionalPropery(), null));
+		editingDomain.getCommandStack().execute(cc);
+		expectedModel.save(Collections.EMPTY_MAP);
 	}
+	/**
+	 * Test the editor properties :
+	 * - init the input model
+	 * - calculate the expected model
+	 * - initialize the model editor
+	 * - change the properties in the editor properties
+	 * - compare the expected and the real model : if they are equals the test pass
+	 * - delete the models
+	 */	
+	public void testRemoveEObjectFlatComboViewerSampleEobjectflatcomboviewerOptionalPropery() throws Exception {
+		
+		// Import the input model
+		initializeInputModel();
+		
+		eObjectFlatComboViewerSample = EEFTestsModelsUtils.getFirstInstanceOf(bot.getActiveResource(), eObjectFlatComboViewerSampleMetaClass);
+		if (eObjectFlatComboViewerSample == null)
+			throw new InputModelInvalidException(eObjectFlatComboViewerSampleMetaClass.getName());
+	
+		// Create the expected model
+		initializeRemoveExpectedModelForEObjectFlatComboViewerSampleEobjectflatcomboviewerOptionalPropery();
+		
+		// Open the input model with the treeview editor
+		SWTBotEditor modelEditor = bot.openActiveModel();
+		
+		// Open the EEF properties view to edit the EObjectFlatComboViewerSample element
+		EObject firstInstanceOf = EEFTestsModelsUtils.getFirstInstanceOf(bot.getActiveResource(), eObjectFlatComboViewerSampleMetaClass);
+		if (firstInstanceOf == null)
+			throw new InputModelInvalidException(eObjectFlatComboViewerSampleMetaClass.getName());
+		
+		SWTBotView propertiesView = bot.prepareLiveEditing(modelEditor, firstInstanceOf);
+		
+		// Change value of the eobjectflatcomboviewerOptionalPropery feature of the EObjectFlatComboViewerSample element 
+		bot.removePropertyEObjectFlatComboViewerFeature(propertiesView, 1, bot.selectNode(modelEditor, firstInstanceOf));
+	
+		
+		// Save the changement
+		bot.finalizeEdition(modelEditor);
+		
+		// Compare real model with expected model
+		assertExpectedModelReached(expectedModel);
+		
+		// Delete the input model
+		deleteModels();
+	
+	}	
 		// FIXME : define 'additionnalMethodsForWidgets' (from widgetTest.mtl) for case (EObjectFlatComboViewer - TotalSample) 
 
 		// FIXME : define 'additionnalMethodsForWidgets' (from widgetTest.mtl) for case (EObjectFlatComboViewer - TotalSample) 
