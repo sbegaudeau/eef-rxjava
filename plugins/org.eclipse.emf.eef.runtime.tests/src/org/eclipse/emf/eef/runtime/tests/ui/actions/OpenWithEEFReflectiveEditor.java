d85e10e9b37126d6755e364e51f5cb093d207931
sbouchet <sbouchet>
2010-03-15 14:01:10 +0000

    code cleanup

--- plugins/org.eclipse.emf.eef.runtime.tests/src/org/eclipse/emf/eef/runtime/tests/ui/actions/OpenWithEEFReflectiveEditor.java
+++ plugins/org.eclipse.emf.eef.runtime.tests/src/org/eclipse/emf/eef/runtime/tests/ui/actions/OpenWithEEFReflectiveEditor.java
@@ -1,3 +1,14 @@
+/**
+ *  Copyright (c) 2008-2010 Obeo.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *  Contributors:
+ *      Obeo - initial API and implementation
+ *
+ */
 package org.eclipse.emf.eef.runtime.tests.ui.actions;
 
 import org.eclipse.core.resources.IFile;
@@ -15,15 +26,15 @@ import org.eclipse.ui.model.WorkbenchLabelProvider;
 import org.eclipse.ui.part.FileEditorInput;
 
 /**
- * Our sample action implements workbench action delegate.
- * The action proxy will be created by the workbench and
- * shown in the UI. When the user tries to use the action,
- * this delegate will be created and execution will be 
- * delegated to it.
+ * Our sample action implements workbench action delegate. The action proxy will be created by the workbench
+ * and shown in the UI. When the user tries to use the action, this delegate will be created and execution
+ * will be delegated to it.
+ * 
  * @see IWorkbenchWindowActionDelegate
  */
 public class OpenWithEEFReflectiveEditor implements IWorkbenchWindowActionDelegate {
 	private IWorkbenchWindow window;
+
 	/**
 	 * The constructor.
 	 */
@@ -31,49 +42,49 @@ public class OpenWithEEFReflectiveEditor implements IWorkbenchWindowActionDelega
 	}
 
 	/**
-	 * The action has been activated. The argument of the
-	 * method represents the 'real' action sitting
-	 * in the workbench UI.
+	 * The action has been activated. The argument of the method represents the 'real' action sitting in the
+	 * workbench UI.
+	 * 
 	 * @see IWorkbenchWindowActionDelegate#run
 	 */
 	public void run(IAction action) {
-		ElementTreeSelectionDialog workspaceJarSelectionDialog =
-			new ElementTreeSelectionDialog(window.getShell(), new WorkbenchLabelProvider(), new WorkbenchContentProvider());
+		ElementTreeSelectionDialog workspaceJarSelectionDialog = new ElementTreeSelectionDialog(window
+				.getShell(), new WorkbenchLabelProvider(), new WorkbenchContentProvider());
 		workspaceJarSelectionDialog.setInput(ResourcesPlugin.getWorkspace().getRoot());
 		workspaceJarSelectionDialog.setTitle("Select the file to open with the EEF Reflective Editor");
 		workspaceJarSelectionDialog.open();
-		
+
 		Object firstResult = workspaceJarSelectionDialog.getFirstResult();
-		
+
 		if (firstResult instanceof IFile)
 			try {
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(new FileEditorInput((IFile) firstResult), "org.eclipse.emf.eef.reflect.editor");
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(
+						new FileEditorInput((IFile)firstResult), "org.eclipse.emf.eef.reflect.editor");
 			} catch (PartInitException e) {
 				EEFRuntimePlugin.getDefault().logError("Unable to open file with EEF Reflective Editor", e);
 			}
 	}
 
 	/**
-	 * Selection in the workbench has been changed. We 
-	 * can change the state of the 'real' action here
-	 * if we want, but this can only happen after 
-	 * the delegate has been created.
+	 * Selection in the workbench has been changed. We can change the state of the 'real' action here if we
+	 * want, but this can only happen after the delegate has been created.
+	 * 
 	 * @see IWorkbenchWindowActionDelegate#selectionChanged
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
 	}
 
 	/**
-	 * We can use this method to dispose of any system
-	 * resources we previously allocated.
+	 * We can use this method to dispose of any system resources we previously allocated.
+	 * 
 	 * @see IWorkbenchWindowActionDelegate#dispose
 	 */
 	public void dispose() {
 	}
 
 	/**
-	 * We will cache window object in order to
-	 * be able to provide parent shell for the message dialog.
+	 * We will cache window object in order to be able to provide parent shell for the message dialog.
+	 * 
 	 * @see IWorkbenchWindowActionDelegate#init
 	 */
 	public void init(IWorkbenchWindow window) {
