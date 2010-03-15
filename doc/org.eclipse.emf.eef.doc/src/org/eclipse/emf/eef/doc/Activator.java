5b8f65c3ba58c2911e1def5fa7f220d7c3f5f93c
sbouchet <sbouchet>
2010-03-15 13:57:17 +0000

    modified header

--- doc/org.eclipse.emf.eef.doc/src/org/eclipse/emf/eef/doc/Activator.java
+++ doc/org.eclipse.emf.eef.doc/src/org/eclipse/emf/eef/doc/Activator.java
@@ -1,3 +1,14 @@
+/**
+ *  Copyright (c) 2010 Obeo.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *  Contributors:
+ *      Obeo - initial API and implementation
+ *
+ */
 package org.eclipse.emf.eef.doc;
 
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -6,6 +17,8 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
+ * 
+ * @author <a href="mailto:stephane.bouchet@obeo.fr">Stephane Bouchet</a>
  */
 public class Activator extends AbstractUIPlugin {
 
@@ -23,7 +36,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * {@inheritDoc}
-	 *
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
@@ -33,7 +46,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * {@inheritDoc}
-	 *
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
