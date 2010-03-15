f2ccd39bdf7cfa5c37edc8387619600c56fc201f
sbouchet <sbouchet>
2010-03-15 14:00:23 +0000

    modified header

--- plugins/org.eclipse.emf.eef.runtime/src/org/eclipse/emf/eef/runtime/impl/utils/EEFRuntimeMessages.java
+++ plugins/org.eclipse.emf.eef.runtime/src/org/eclipse/emf/eef/runtime/impl/utils/EEFRuntimeMessages.java
@@ -1,15 +1,33 @@
+/**
+ *  Copyright (c) 2008-2010 Obeo.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *  Contributors:
+ *      Obeo - initial API and implementation
+ *
+ */
 package org.eclipse.emf.eef.runtime.impl.utils;
 
 import org.eclipse.osgi.util.NLS;
 
 public class EEFRuntimeMessages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.emf.eef.runtime.impl.utils.eefRuntimeMessages"; //$NON-NLS-1$
+
 	public static String ComposedPropertiesEditionComponent_cmd_not_performed;
+
 	public static String ComposedPropertiesEditionComponent_eef_validation_diagnostic;
+
 	public static String ComposedPropertiesEditionComponent_no_problem;
+
 	public static String CurrentModelViewerFilter_name;
+
 	public static String ModelViewerHelper_name_feature;
+
 	public static String ReferencedModelsViewerFilter_name;
+
 	public static String StringTools_color_delim;
 	static {
 		// initialize resource bundle
