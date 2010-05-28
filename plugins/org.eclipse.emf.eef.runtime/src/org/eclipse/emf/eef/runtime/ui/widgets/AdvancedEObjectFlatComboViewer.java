36056588c7c868690dd9645677a64bf89e405c48
sbouchet <sbouchet>
2010-05-28 12:43:51 +0000

    fixed copyrights

--- plugins/org.eclipse.emf.eef.runtime/src/org/eclipse/emf/eef/runtime/ui/widgets/AdvancedEObjectFlatComboViewer.java
+++ plugins/org.eclipse.emf.eef.runtime/src/org/eclipse/emf/eef/runtime/ui/widgets/AdvancedEObjectFlatComboViewer.java
@@ -1,17 +1,14 @@
-/*****************************************************************************
- * Copyright (c) 2008-2009 CEA LIST, Obeo
- *
- *    
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 CEA LIST and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
- *  Obeo
- *
- *****************************************************************************/
+ *     Patrick Tessier (CEA LIST) Patrick.tessier@cea.fr - Initial API and implementation
+ *     Obeo - Some improvements
+ *******************************************************************************/
 package org.eclipse.emf.eef.runtime.ui.widgets;
 
 import java.util.ArrayList;
