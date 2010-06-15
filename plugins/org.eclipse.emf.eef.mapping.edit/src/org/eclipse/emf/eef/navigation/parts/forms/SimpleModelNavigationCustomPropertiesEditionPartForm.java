ab9ff3d5cea4b5edc9daa3606dc932933198d6af
sbouchet <sbouchet>
2010-06-15 08:53:48 +0000

    fixed copyrights

--- plugins/org.eclipse.emf.eef.mapping.edit/src/org/eclipse/emf/eef/navigation/parts/forms/SimpleModelNavigationCustomPropertiesEditionPartForm.java
+++ plugins/org.eclipse.emf.eef.mapping.edit/src/org/eclipse/emf/eef/navigation/parts/forms/SimpleModelNavigationCustomPropertiesEditionPartForm.java
@@ -1,16 +1,13 @@
-/**
- *  Copyright (c) 2008-2009 Obeo.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- *  
- *  Contributors:
- *      Obeo - initial API and implementation
- * 
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: SimpleModelNavigationCustomPropertiesEditionPartForm.java,v 1.2 2010/03/15 13:58:10 sbouchet Exp $
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.navigation.parts.forms;
 
 import org.eclipse.emf.ecore.EReference;
