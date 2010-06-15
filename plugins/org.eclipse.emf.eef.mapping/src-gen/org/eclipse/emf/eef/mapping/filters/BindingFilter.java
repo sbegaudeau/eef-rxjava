2e9f0b5ad07760a3d818c96ec8c8585079dbb374
sbouchet <sbouchet>
2010-06-15 08:39:47 +0000

    fixed copyrights

--- plugins/org.eclipse.emf.eef.mapping/src-gen/org/eclipse/emf/eef/mapping/filters/BindingFilter.java
+++ plugins/org.eclipse.emf.eef.mapping/src-gen/org/eclipse/emf/eef/mapping/filters/BindingFilter.java
@@ -1,16 +1,13 @@
-/**
- *  Copyright (c) 2008 Obeo.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- *  
- *  Contributors:
- *      Obeo - initial API and implementation
- * 
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: BindingFilter.java,v 1.2 2009/05/26 08:20:04 glefur Exp $
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.mapping.filters;
 
 import org.eclipse.emf.eef.mapping.DocumentedElement;
--- plugins/org.eclipse.emf.eef.mapping/src-gen/org/eclipse/emf/eef/mapping/filters/JavaFilter.java
+++ plugins/org.eclipse.emf.eef.mapping/src-gen/org/eclipse/emf/eef/mapping/filters/JavaFilter.java
@@ -1,16 +1,13 @@
-/**
- *  Copyright (c) 2008 Obeo.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- *  
- *  Contributors:
- *      Obeo - initial API and implementation
- * 
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: JavaFilter.java,v 1.1 2009/05/19 16:58:32 sbouchet Exp $
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.mapping.filters;
 
 /**
