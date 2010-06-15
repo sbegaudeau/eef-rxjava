6fc1fa8e5924951185e3ba811ad5afeb4e873b73
sbouchet <sbouchet>
2010-06-15 08:36:51 +0000

    fixed copyrights

--- plugins/org.eclipse.emf.eef.eefgen.editor/src-gen/org/eclipse/emf/eef/EEFGen/presentation/EEFGenEditor.java
+++ plugins/org.eclipse.emf.eef.eefgen.editor/src-gen/org/eclipse/emf/eef/EEFGen/presentation/EEFGenEditor.java
@@ -1,16 +1,13 @@
-/**
- *  Copyright (c) 2008 Obeo.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- *  
- *  Contributors:
- *      Obeo - initial API and implementation
- * 
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: EEFGenEditor.java,v 1.6 2009/12/11 11:05:37 sbouchet Exp $
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.EEFGen.presentation;
 
 
