6a21874f8b937772caaa5cddd0876546a835eaf7
sbouchet <sbouchet>
2010-06-15 08:30:23 +0000

    fixed copyrights

--- plugins/org.eclipse.emf.eef.components/src-gen/org/eclipse/emf/eef/components/impl/PropertiesEditionComponentImpl.java
+++ plugins/org.eclipse.emf.eef.components/src-gen/org/eclipse/emf/eef/components/impl/PropertiesEditionComponentImpl.java
@@ -1,16 +1,13 @@
-/**
- *  Copyright (c) 2008 Obeo.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- *  
- *  Contributors:
- *      Obeo - initial API and implementation
- * 
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: PropertiesEditionComponentImpl.java,v 1.3 2009/09/10 09:18:18 sbouchet Exp $
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.components.impl;
 
 import org.eclipse.emf.common.notify.Notification;
