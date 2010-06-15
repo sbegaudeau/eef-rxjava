c1d0ef827ddcfadb411a7d958b32918367c9d3e3
sbouchet <sbouchet>
2010-06-15 08:34:30 +0000

    fixed copyrights

--- plugins/org.eclipse.emf.eef.eefgen/src-gen/org/eclipse/emf/eef/EEFGen/impl/EEFGenModelReferenceImpl.java
+++ plugins/org.eclipse.emf.eef.eefgen/src-gen/org/eclipse/emf/eef/EEFGen/impl/EEFGenModelReferenceImpl.java
@@ -1,16 +1,13 @@
-/**
- *  Copyright (c) 2008 Obeo.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- *  
- *  Contributors:
- *      Obeo - initial API and implementation
- * 
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: EEFGenModelReferenceImpl.java,v 1.3 2010/03/15 10:06:03 sbouchet Exp $
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.EEFGen.impl;
 
 import org.eclipse.emf.common.notify.Notification;
