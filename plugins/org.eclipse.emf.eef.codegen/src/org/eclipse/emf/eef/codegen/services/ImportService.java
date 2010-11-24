abd68917597b7f86a3d8a59ac5ad2cb6f3488186
glefur <glefur>
2010-11-24 16:31:47 +0000

    FIXED: viewsReference bugs CompositionStep naming

---
+++ plugins/org.eclipse.emf.eef.codegen/src/org/eclipse/emf/eef/codegen/services/ImportService.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.eef.codegen.services;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * @author <a href="mailto:goulwen.lefur@obeo.fr">Goulwen Le Fur</a>
+ *
+ */
+public class ImportService {
+
+    public void addImport(EObject current, String importNamespace) {
+        ImportMarker.getOrCreateMarker(current).add(importNamespace);
+    }
+
+    public String getImports(EObject cur) {
+        String importString = ImportMarker.getOrCreateMarker(cur).toString();
+        ImportMarker.getOrCreateMarker(cur).clear();
+        return importString;
+    }
+
+}
+
