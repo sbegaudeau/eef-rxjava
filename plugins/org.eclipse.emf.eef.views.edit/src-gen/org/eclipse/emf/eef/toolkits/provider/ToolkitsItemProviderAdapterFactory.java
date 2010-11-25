f65342a542797ce08838a2ddac8ae5a71b4e9c6d
glefur <glefur>
2010-11-25 08:25:39 +0000

    UPDATED: Setting metamodel as extensible

--- plugins/org.eclipse.emf.eef.views.edit/src-gen/org/eclipse/emf/eef/toolkits/provider/ToolkitsItemProviderAdapterFactory.java
+++ plugins/org.eclipse.emf.eef.views.edit/src-gen/org/eclipse/emf/eef/toolkits/provider/ToolkitsItemProviderAdapterFactory.java
@@ -13,13 +13,18 @@ package org.eclipse.emf.eef.toolkits.provider;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import java.util.List;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.ChangeNotifier;
+import org.eclipse.emf.edit.provider.ChildCreationExtenderManager;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.provider.IChangeNotifier;
+import org.eclipse.emf.edit.provider.IChildCreationExtender;
 import org.eclipse.emf.edit.provider.IDisposable;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -27,7 +32,9 @@ import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.INotifyChangedListener;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.eef.toolkits.ToolkitsPackage;
 import org.eclipse.emf.eef.toolkits.util.ToolkitsAdapterFactory;
+import org.eclipse.emf.eef.views.provider.ViewsEditPlugin;
 
 /**
  * This is the factory that is used to provide the interfaces needed to support Viewers.
@@ -38,7 +45,7 @@ import org.eclipse.emf.eef.toolkits.util.ToolkitsAdapterFactory;
  * <!-- end-user-doc -->
  * @generated
  */
-public class ToolkitsItemProviderAdapterFactory extends ToolkitsAdapterFactory implements ComposeableAdapterFactory, IChangeNotifier, IDisposable {
+public class ToolkitsItemProviderAdapterFactory extends ToolkitsAdapterFactory implements ComposeableAdapterFactory, IChangeNotifier, IDisposable, IChildCreationExtender {
 	/**
 	 * This keeps track of the root adapter factory that delegates to this adapter factory.
 	 * <!-- begin-user-doc -->
@@ -54,6 +61,14 @@ public class ToolkitsItemProviderAdapterFactory extends ToolkitsAdapterFactory i
 	 * @generated
 	 */
 	protected IChangeNotifier changeNotifier = new ChangeNotifier();
+
+	/**
+	 * This helps manage the child creation extenders.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ChildCreationExtenderManager childCreationExtenderManager = new ChildCreationExtenderManager(ViewsEditPlugin.INSTANCE, ToolkitsPackage.eNS_URI);
 
 	/**
 	 * This keeps track of all the supported types checked by {@link #isFactoryForType isFactoryForType}.
@@ -179,6 +194,33 @@ public class ToolkitsItemProviderAdapterFactory extends ToolkitsAdapterFactory i
 		}
 
 		return null;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public List<IChildCreationExtender> getChildCreationExtenders() {
+		return childCreationExtenderManager.getChildCreationExtenders();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Collection<?> getNewChildDescriptors(Object object, EditingDomain editingDomain) {
+		return childCreationExtenderManager.getNewChildDescriptors(object, editingDomain);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ResourceLocator getResourceLocator() {
+		return childCreationExtenderManager;
 	}
 
 	/**
