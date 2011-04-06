77d008590c82a5a0257d30712291c4d6be683b5a
nlepine <nlepine>
2011-04-06 13:09:23 +0000

    add modeling bot

---
+++ plugins/org.eclipse.emf.eef.modelingbot.editor/src-gen/org/eclipse/emf/eef/modelingBot/presentation/MbotEditorPlugin.java
@@ -0,0 +1,113 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: MbotEditorPlugin.java,v 1.1 2011/04/06 13:09:23 nlepine Exp $
+ */
+package org.eclipse.emf.eef.modelingBot.presentation;
+
+import org.eclipse.emf.codegen.ecore.genmodel.provider.GenModelEditPlugin;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
+
+import org.eclipse.emf.eef.components.provider.ComponentsEditPlugin;
+
+import org.eclipse.emf.eef.extended.editor.provider.ExtendedEditPlugin;
+
+import org.eclipse.emf.eef.mapping.provider.MappingEditPlugin;
+
+import org.eclipse.emf.eef.views.provider.ViewsEditPlugin;
+
+/**
+ * This is the central singleton for the Mbot editor plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class MbotEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final MbotEditorPlugin INSTANCE = new MbotEditorPlugin();
+	
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public MbotEditorPlugin() {
+		super
+			(new ResourceLocator [] {
+				ComponentsEditPlugin.INSTANCE,
+				EcoreEditPlugin.INSTANCE,
+				ExtendedEditPlugin.INSTANCE,
+				GenModelEditPlugin.INSTANCE,
+				MappingEditPlugin.INSTANCE,
+				ViewsEditPlugin.INSTANCE,
+			});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+	
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
