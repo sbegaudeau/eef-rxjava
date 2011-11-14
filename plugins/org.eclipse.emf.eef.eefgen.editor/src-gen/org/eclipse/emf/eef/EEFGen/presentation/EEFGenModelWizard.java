b08ebbe5af6f4b4eaec733d2a34d82ec94870b38
sbouchet <sbouchet>
2011-11-14 13:59:10 +0000

    emf regen

--- plugins/org.eclipse.emf.eef.eefgen.editor/src-gen/org/eclipse/emf/eef/EEFGen/presentation/EEFGenModelWizard.java
+++ plugins/org.eclipse.emf.eef.eefgen.editor/src-gen/org/eclipse/emf/eef/EEFGen/presentation/EEFGenModelWizard.java
@@ -272,7 +272,7 @@ public class EEFGenModelWizard extends Wizard implements INewWizard {
 			try {
 				page.openEditor
 					(new FileEditorInput(modelFile),
-					 workbench.getEditorRegistry().getDefaultEditor(modelFile.getFullPath().toString()).getId());
+					 workbench.getEditorRegistry().getDefaultEditor(modelFile.getFullPath().toString()).getId());					 	 
 			}
 			catch (PartInitException exception) {
 				MessageDialog.openError(workbenchWindow.getShell(), EEFGenEditorPlugin.INSTANCE.getString("_UI_OpenEditorError_label"), exception.getMessage());
