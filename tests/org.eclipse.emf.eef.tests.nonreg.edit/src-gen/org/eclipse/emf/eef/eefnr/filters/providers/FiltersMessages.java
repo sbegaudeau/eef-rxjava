2f844ee77540d71ffb3b3a963a77be76138c98cd
sbouchet <sbouchet>
2011-11-16 08:20:29 +0000

    added testcases

--- tests/org.eclipse.emf.eef.tests.nonreg.edit/src-gen/org/eclipse/emf/eef/eefnr/filters/providers/FiltersMessages.java
+++ tests/org.eclipse.emf.eef.tests.nonreg.edit/src-gen/org/eclipse/emf/eef/eefnr/filters/providers/FiltersMessages.java
@@ -111,8 +111,8 @@ public class FiltersMessages extends NLS {
 	public static String PropertiesEditionPart_RemoveListViewerLabel;
 
 	// Start of user code for additionnal NLS Constants
-
-// End of user code
+	
+	// End of user code
 
 	static {
 		// initialize resource bundle
