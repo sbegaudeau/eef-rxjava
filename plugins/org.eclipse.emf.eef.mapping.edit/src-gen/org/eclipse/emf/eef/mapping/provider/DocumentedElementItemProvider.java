1f1e2692b9d7d8c3dd143ec9b4a5fd460f0f4c54
sbouchet <sbouchet>
2012-03-21 16:02:09 +0000

    fixed copyright headers

--- plugins/org.eclipse.emf.eef.mapping.edit/src-gen/org/eclipse/emf/eef/mapping/provider/DocumentedElementItemProvider.java
+++ plugins/org.eclipse.emf.eef.mapping.edit/src-gen/org/eclipse/emf/eef/mapping/provider/DocumentedElementItemProvider.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
+/*******************************************************************************
+ * Copyright (c) 2008, 2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: DocumentedElementItemProvider.java,v 1.16 2012/03/16 16:07:34 sbouchet Exp $
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.eef.mapping.provider;
 
 import java.util.Collection;
