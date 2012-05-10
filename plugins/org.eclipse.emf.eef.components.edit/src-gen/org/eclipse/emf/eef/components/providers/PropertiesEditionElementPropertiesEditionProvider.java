d135a94de68d89f854fb07f4195299b0f78dcda1
glefur <glefur>
2012-05-10 16:32:30 +0000

    UPDATED: generation

--- plugins/org.eclipse.emf.eef.components.edit/src-gen/org/eclipse/emf/eef/components/providers/PropertiesEditionElementPropertiesEditionProvider.java
+++ plugins/org.eclipse.emf.eef.components.edit/src-gen/org/eclipse/emf/eef/components/providers/PropertiesEditionElementPropertiesEditionProvider.java
@@ -26,6 +26,8 @@ import org.eclipse.emf.eef.runtime.api.component.IPropertiesEditionComponent;
 
 import org.eclipse.emf.eef.runtime.context.PropertiesEditingContext;
 
+import org.eclipse.emf.eef.runtime.impl.utils.EEFUtils;
+
 import org.eclipse.emf.eef.runtime.providers.PropertiesEditingProvider;
 
 import org.eclipse.emf.eef.runtime.providers.impl.PropertiesEditingProviderImpl;
@@ -147,7 +149,8 @@ public class PropertiesEditionElementPropertiesEditionProvider extends Propertie
 		 * @see org.eclipse.jface.viewers.IFilter#select(java.lang.Object)
 		 */
 		public boolean select(Object toTest) {
-			return toTest instanceof EObject && ComponentsPackage.Literals.PROPERTIES_EDITION_ELEMENT == ((EObject)toTest).eClass();
+			EObject eObj = EEFUtils.resolveSemanticObject(toTest);
+			return eObj != null && ComponentsPackage.Literals.PROPERTIES_EDITION_ELEMENT == eObj.eClass();
 		}
 		
 	}
