2bcebe1f57e7ba099e3920737a52e4ddb6132b9e
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2012-11-30 18:21:22 +0100

    UPDATED: trying to add jdt to SWTbot launch configuration in order to access java perspective.

--- tests/org.eclipse.emf.eef.tests.nonreg.junit/src-mbot/org/eclipse/emf/eef/tests/nonreg/AllTests.java
+++ tests/org.eclipse.emf.eef.tests.nonreg.junit/src-mbot/org/eclipse/emf/eef/tests/nonreg/AllTests.java
@@ -34,6 +34,34 @@ import org.eclipse.emf.eef.tests.nonreg.batch.detailsview.BatchMBotUnsetReferenc
 import org.eclipse.emf.eef.tests.nonreg.batch.detailsview.BatchMBotUnsetReferenceFlatRefTable;
 import org.eclipse.emf.eef.tests.nonreg.batch.detailsview.BatchMBotUnsetReferenceRefTable;
 import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotAddAdvTableCompoOnAbstractRef;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotAddTableCompo;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotCancelAddTableCompo;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotCancelSetReferenceAEOFCV;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotCancelSetReferenceFlatRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotCancelSetReferenceRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotRedoAddTableCompo;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotRedoSetAttributeRadio;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotRedoSetReferenceAEOFCV;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotRedoSetReferenceFlatRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotRedoSetReferenceRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotRemoveTableCompo;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotSetAttributeRadio;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotSetReferenceAEOFCV;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotSetReferenceFlatRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotSetReferenceRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUndoAddTableCompo;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUndoSetAttributeRadio;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUndoSetReferenceAEOFCV;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUndoSetReferenceFlatRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUndoSetReferenceRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUnsetAEOFCV;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUnsetEOFCV;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUnsetFlatRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUnsetRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUnsetReferenceAEOFCV;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUnsetReferenceEOFCV;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUnsetReferenceFlatRefTable;
+import org.eclipse.emf.eef.tests.nonreg.detailsview.MBotUnsetReferenceRefTable;
 
 public class AllTests extends TestCase {
 
@@ -71,8 +99,35 @@ public class AllTests extends TestCase {
 		suite.addTestSuite(BatchMBotUnsetRefTable.class);
 		
 		// Details
-		suite.addTestSuite(MBotAddAdvTableCompoOnAbstractRef.class);		
-		
+		suite.addTestSuite(MBotAddAdvTableCompoOnAbstractRef.class);
+		suite.addTestSuite(MBotAddTableCompo.class);
+		suite.addTestSuite(MBotCancelAddTableCompo.class);
+		suite.addTestSuite(MBotCancelSetReferenceAEOFCV.class);
+		suite.addTestSuite(MBotCancelSetReferenceFlatRefTable.class);
+		suite.addTestSuite(MBotCancelSetReferenceRefTable.class);
+		suite.addTestSuite(MBotRedoAddTableCompo.class);
+		suite.addTestSuite(MBotRedoSetAttributeRadio.class);
+		suite.addTestSuite(MBotRedoSetReferenceAEOFCV.class);
+		suite.addTestSuite(MBotRedoSetReferenceFlatRefTable.class);
+		suite.addTestSuite(MBotRedoSetReferenceRefTable.class);
+		suite.addTestSuite(MBotRemoveTableCompo.class);
+		suite.addTestSuite(MBotSetAttributeRadio.class);
+		suite.addTestSuite(MBotSetReferenceAEOFCV.class);
+		suite.addTestSuite(MBotSetReferenceFlatRefTable.class);
+		suite.addTestSuite(MBotSetReferenceRefTable.class);
+		suite.addTestSuite(MBotUndoAddTableCompo.class);
+		suite.addTestSuite(MBotUndoSetAttributeRadio.class);
+		suite.addTestSuite(MBotUndoSetReferenceAEOFCV.class);
+		suite.addTestSuite(MBotUndoSetReferenceFlatRefTable.class);
+		suite.addTestSuite(MBotUndoSetReferenceRefTable.class);
+		suite.addTestSuite(MBotUnsetAEOFCV.class);
+		suite.addTestSuite(MBotUnsetEOFCV.class);
+		suite.addTestSuite(MBotUnsetFlatRefTable.class);
+		suite.addTestSuite(MBotUnsetReferenceAEOFCV.class);
+		suite.addTestSuite(MBotUnsetReferenceEOFCV.class);
+//		suite.addTestSuite(MBotUnsetReferenceFlatRefTable.class);
+//		suite.addTestSuite(MBotUnsetReferenceRefTable.class);
+//		suite.addTestSuite(MBotUnsetRefTable.class);
 		//$JUnit-END$
 		return suite;
 	}
