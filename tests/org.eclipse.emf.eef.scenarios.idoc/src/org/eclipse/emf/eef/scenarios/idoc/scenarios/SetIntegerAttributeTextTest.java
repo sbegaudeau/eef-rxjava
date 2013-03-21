a799a67fbf3335cbf6875d701fbfe645e9852261
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2013-03-21 16:14:44 +0100

    ADDED: First example of DDT

---
+++ tests/org.eclipse.emf.eef.scenarios.idoc/src/org/eclipse/emf/eef/scenarios/idoc/scenarios/SetIntegerAttributeTextTest.java
@@ -0,0 +1,9 @@
+package org.eclipse.emf.eef.scenarios.idoc.scenarios;
+/**
+* Tests the 'setIntegerAttributeText' Scenario.
+*/
+public class SetIntegerAttributeTextTest extends org.eclipse.emf.eef.modelingBot.testcase.AbstractComposedModelingBotTestCase {
+	public void testModelingBot() throws Exception {
+		bot.runModelingBot("intent:/org.eclipse.emf.eef.scenarios.idoc/setIntegerAttributeText.modelingbot");
+	}
+}
