d08acf29b7031828fe0089228463c1f427b112a8
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2015-12-10 10:05:51 +0100

    [build] Improve the build
    1) Add org.eclipse.eef.properties.ui to the build
    2) Fix an issue with a missing maven plugin version for an update site
    3) Fix an error in a unit test when run as an Eclipse Plugin test
    
    Change-Id: I16b852fd6fc89980eba286024fbe3c7b91c099c6
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

--- tests/org.eclipse.eef.tests/src/org/eclipse/eef/tests/internal/controllers/EEFTextControllerTests.java
+++ tests/org.eclipse.eef.tests/src/org/eclipse/eef/tests/internal/controllers/EEFTextControllerTests.java
@@ -19,13 +19,17 @@ import org.eclipse.eef.core.api.EEFVariableManagerFactory;
 import org.eclipse.eef.core.api.IVariableManager;
 import org.eclipse.eef.core.api.controllers.EEFTextController;
 import org.eclipse.eef.core.internal.controllers.EEFTextControllerImpl;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CommandStack;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.impl.TransactionalEditingDomainImpl;
 import org.eclipse.sirius.common.interpreter.api.IInterpreter;
 import org.junit.Before;
@@ -65,6 +69,8 @@ public class EEFTextControllerTests {
 	public void setUp() {
 		AdapterFactory adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
 		this.resourceSet = new ResourceSetImpl();
+		this.resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
+		.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
 		this.editingDomain = new TransactionalEditingDomainImpl(adapterFactory, this.resourceSet);
 		this.variableManager = new EEFVariableManagerFactory().createVariableManager();
 
@@ -99,7 +105,15 @@ public class EEFTextControllerTests {
 		eClass.setName(name);
 
 		Resource resource = this.resourceSet.createResource(URI.createURI("test.ecore")); //$NON-NLS-1$
-		resource.getContents().add(eClass);
+		Command command = new RecordingCommand(this.editingDomain) {
+			@Override
+			protected void doExecute() {
+				resource.getContents().add(eClass);
+			}
+		};
+
+		CommandStack commandStack = this.editingDomain.getCommandStack();
+		commandStack.execute(command);
 
 		this.variableManager.getVariables().put(EEFExpressionUtils.SELF, eClass);
 	}
