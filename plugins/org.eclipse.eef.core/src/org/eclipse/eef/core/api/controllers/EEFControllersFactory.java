eclipse-eef/org.eclipse.eef

Add support for multiple groups in an IEEFSection.
1) An EEFSection is now initialized with an EEFPage
2) EEFGroups can now react to the various events available
(aboutToBeShown, refresh, aboutToBeHidden, etc)

Change-Id: I7f33936543b1b88b922fe92f509ef6fcc1629030
Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

plugins/org.eclipse.eef.core/src/org/eclipse/eef/core/api/controllers/EEFControllersFactory.java
@@ -11,10 +11,12 @@
 package org.eclipse.eef.core.api.controllers;
 
 import org.eclipse.eef.EEFCheckboxDescription;
+import org.eclipse.eef.EEFGroupDescription;
 import org.eclipse.eef.EEFLabelDescription;
 import org.eclipse.eef.EEFSelectDescription;
 import org.eclipse.eef.EEFTextDescription;
 import org.eclipse.eef.core.internal.controllers.EEFCheckboxControllerImpl;
+import org.eclipse.eef.core.internal.controllers.EEFGroupControllerImpl;
 import org.eclipse.eef.core.internal.controllers.EEFLabelControllerImpl;
 import org.eclipse.eef.core.internal.controllers.EEFSelectControllerImpl;
 import org.eclipse.eef.core.internal.controllers.EEFTextControllerImpl;
@@ -28,6 +30,22 @@ import org.eclipse.sirius.common.interpreter.api.IVariableManager;
  * @author sbegaudeau
  */
 public class EEFControllersFactory {
+
+	/**
+	 * Creates a new group controller.
+	 *
+	 * @param description
+	 *            The description
+	 * @param variableManager
+	 *            The variable manager
+	 * @param interpreter
+	 *            The interpreter
+	 * @return The group controller
+	 */
+	public IEEFGroupController createGroupController(EEFGroupDescription description, IVariableManager variableManager, IInterpreter interpreter) {
+		return new EEFGroupControllerImpl(description, variableManager, interpreter);
+	}
+
 	/**
 	 * Creates a new text controller.
 	 *
@@ -41,7 +59,7 @@ public class EEFControllersFactory {
 	 *            The editing domain
 	 * @return A text controller
 	 */
-	public EEFTextController createTextController(EEFTextDescription description, IVariableManager variableManager, IInterpreter interpreter,
+	public IEEFTextController createTextController(EEFTextDescription description, IVariableManager variableManager, IInterpreter interpreter,
 			TransactionalEditingDomain editingDomain) {
 		return new EEFTextControllerImpl(description, variableManager, interpreter, editingDomain);
 	}
@@ -57,7 +75,7 @@ public class EEFControllersFactory {
 	 *            The interpreter
 	 * @return A label controller
 	 */
-	public EEFLabelController createLabelController(EEFLabelDescription description, IVariableManager variableManager, IInterpreter interpreter) {
+	public IEEFLabelController createLabelController(EEFLabelDescription description, IVariableManager variableManager, IInterpreter interpreter) {
 		return new EEFLabelControllerImpl(description, variableManager, interpreter);
 	}
 
@@ -74,7 +92,7 @@ public class EEFControllersFactory {
 	 *            The editing domain
 	 * @return A label controller
 	 */
-	public EEFSelectController createSelectController(EEFSelectDescription description, IVariableManager variableManager, IInterpreter interpreter,
+	public IEEFSelectController createSelectController(EEFSelectDescription description, IVariableManager variableManager, IInterpreter interpreter,
 			TransactionalEditingDomain editingDomain) {
 		return new EEFSelectControllerImpl(description, variableManager, interpreter, editingDomain);
 	}
@@ -92,7 +110,7 @@ public class EEFControllersFactory {
 	 *            The editing domain
 	 * @return A checkbox controller
 	 */
-	public EEFCheckboxController createCheckboxController(EEFCheckboxDescription description, IVariableManager variableManager,
+	public IEEFCheckboxController createCheckboxController(EEFCheckboxDescription description, IVariableManager variableManager,
 			IInterpreter interpreter, TransactionalEditingDomain editingDomain) {
 		return new EEFCheckboxControllerImpl(description, variableManager, interpreter, editingDomain);
 

sbegaudeau/eef-rxjava

plugins/org.eclipse.eef.core/src/org/eclipse/eef/core/api/controllers/EEFControllersFactory.java
@@ -11,13 +11,15 @@
 package org.eclipse.eef.core.api.controllers;
 
 import org.eclipse.eef.EEFCheckboxDescription;
+import org.eclipse.eef.EEFGroupDescription;
 import org.eclipse.eef.EEFLabelDescription;
 import org.eclipse.eef.EEFSelectDescription;
 import org.eclipse.eef.EEFTextDescription;
-import org.eclipse.eef.core.internal.controllers.EEFCheckboxControllerImpl;
-import org.eclipse.eef.core.internal.controllers.EEFLabelControllerImpl;
-import org.eclipse.eef.core.internal.controllers.EEFSelectControllerImpl;
-import org.eclipse.eef.core.internal.controllers.EEFTextControllerImpl;
+import org.eclipse.eef.core.internal.controllers.EEFCheckboxController;
+import org.eclipse.eef.core.internal.controllers.EEFGroupController;
+import org.eclipse.eef.core.internal.controllers.EEFLabelController;
+import org.eclipse.eef.core.internal.controllers.EEFSelectController;
+import org.eclipse.eef.core.internal.controllers.EEFTextController;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.sirius.common.interpreter.api.IInterpreter;
 import org.eclipse.sirius.common.interpreter.api.IVariableManager;
@@ -28,6 +30,22 @@ import org.eclipse.sirius.common.interpreter.api.IVariableManager;
  * @author sbegaudeau
  */
 public class EEFControllersFactory {
+
+	/**
+	 * Creates a new group controller.
+	 *
+	 * @param description
+	 *            The description
+	 * @param variableManager
+	 *            The variable manager
+	 * @param interpreter
+	 *            The interpreter
+	 * @return The group controller
+	 */
+	public IEEFGroupController createGroupController(EEFGroupDescription description, IVariableManager variableManager, IInterpreter interpreter) {
+		return new EEFGroupController(description, variableManager, interpreter);
+	}
+
 	/**
 	 * Creates a new text controller.
 	 *
@@ -41,9 +59,9 @@ public class EEFControllersFactory {
 	 *            The editing domain
 	 * @return A text controller
 	 */
-	public EEFTextController createTextController(EEFTextDescription description, IVariableManager variableManager, IInterpreter interpreter,
+	public IEEFTextController createTextController(EEFTextDescription description, IVariableManager variableManager, IInterpreter interpreter,
 			TransactionalEditingDomain editingDomain) {
-		return new EEFTextControllerImpl(description, variableManager, interpreter, editingDomain);
+		return new EEFTextController(description, variableManager, interpreter, editingDomain);
 	}
 
 	/**
@@ -57,8 +75,8 @@ public class EEFControllersFactory {
 	 *            The interpreter
 	 * @return A label controller
 	 */
-	public EEFLabelController createLabelController(EEFLabelDescription description, IVariableManager variableManager, IInterpreter interpreter) {
-		return new EEFLabelControllerImpl(description, variableManager, interpreter);
+	public IEEFLabelController createLabelController(EEFLabelDescription description, IVariableManager variableManager, IInterpreter interpreter) {
+		return new EEFLabelController(description, variableManager, interpreter);
 	}
 
 	/**
@@ -74,9 +92,9 @@ public class EEFControllersFactory {
 	 *            The editing domain
 	 * @return A label controller
 	 */
-	public EEFSelectController createSelectController(EEFSelectDescription description, IVariableManager variableManager, IInterpreter interpreter,
+	public IEEFSelectController createSelectController(EEFSelectDescription description, IVariableManager variableManager, IInterpreter interpreter,
 			TransactionalEditingDomain editingDomain) {
-		return new EEFSelectControllerImpl(description, variableManager, interpreter, editingDomain);
+		return new EEFSelectController(description, variableManager, interpreter, editingDomain);
 	}
 
 	/**
@@ -92,9 +110,9 @@ public class EEFControllersFactory {
 	 *            The editing domain
 	 * @return A checkbox controller
 	 */
-	public EEFCheckboxController createCheckboxController(EEFCheckboxDescription description, IVariableManager variableManager,
+	public IEEFCheckboxController createCheckboxController(EEFCheckboxDescription description, IVariableManager variableManager,
 			IInterpreter interpreter, TransactionalEditingDomain editingDomain) {
-		return new EEFCheckboxControllerImpl(description, variableManager, interpreter, editingDomain);
+		return new EEFCheckboxController(description, variableManager, interpreter, editingDomain);
 
 	}
 }